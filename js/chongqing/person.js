3b06c5ce0a6e75683e6f35434d460b2df570bf72
Albert-cyou <zhangchen2123@mails.jlu.edu.cn>
2025-05-10 20:01:42 +0800

    修改了重庆的部分内容，添加个人主页（未完全完善）

---
+++ js/chongqing/person.js
@@ -0,0 +1,16 @@
+const reveal = () => {
+    const reveals = document.querySelectorAll('.reveal');
+    
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const elementVisible = 150;
+        
+        if (elementTop < windowHeight - elementVisible) {
+            element.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', reveal);
+reveal(); // 初始检查
